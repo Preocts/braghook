bf9025fbb77de9e769f85f954895903dd3a5b8f9
Preocts <preocts@gmail.com>
2022-12-20 21:01:50 -0500

    chore: merge webhook_builder.py back into braghook.py

--- src/braghook/braghook.py
+++ src/braghook/braghook.py
@@ -8,6 +8,7 @@ import dataclasses
 import http.client
 import json
 import logging
+import re
 import subprocess
 from configparser import ConfigParser
 from datetime import datetime
@@ -15,10 +16,20 @@ from pathlib import Path
 from typing import Any
 from typing import TYPE_CHECKING
 
-from braghook import webhook_builder
-
 if TYPE_CHECKING:
-    ...
+    from typing import Protocol
+
+    class Builder(Protocol):
+        """Protocol for the message builder."""
+
+        def __call__(
+            self,
+            author: str,
+            author_icon: str,
+            content: str,
+        ) -> dict[str, Any]:
+            ...
+
 
 DEFAULT_CONFIG_FILE = "braghook.ini"
 DEFAULT_FILE_TEMPLATE = """### {date}
@@ -119,7 +130,7 @@ def read_file_contents(filename: str) -> str:
 
 def send_message(config: Config, content: str) -> None:
     """Send the message to any webhooks defined in config."""
-    for config_field, builder in webhook_builder.BUILDERS.items():
+    for config_field, builder in BUILDERS.items():
         url = getattr(config, config_field)
         if not url:
             continue  # Skip if the webhook is not defined in config
@@ -178,6 +189,156 @@ def post_brag_to_gist(config: Config, filename: str, content: str) -> None:
         logger.error("Error sending gist: %s", response.read())
 
 
+def extract_title_from_message(message: str) -> str:
+    """Extract the title from the message."""
+    match = re.search(r"^#{1,4}\s(.+)$", message, re.MULTILINE)
+    return match.group(1).strip() if match else ""
+
+
+def bullet_marks_to_diamonds(message: str) -> str:
+    """Convert bullet marks to diamonds."""
+    message = re.sub(r"^[-*]\s?", r":small_blue_diamond: ", message, flags=re.MULTILINE)
+    message = re.sub(
+        r"^(\s*)[-*]\s?", r":small_orange_diamond: ", message, flags=re.MULTILINE
+    )
+    return message
+
+
+def headers_to_bold(message: str) -> str:
+    """Convert headers to bold."""
+    message = re.sub(r"^#{1,4}\s(.+)$", r"**\1**", message, flags=re.MULTILINE)
+    return message
+
+
+def build_discord_webhook_plain(
+    author: str,
+    author_icon: str,
+    content: str,
+) -> dict[str, Any]:
+    """Build the Discord webhook."""
+    content = f"{author} ({author_icon})\n{content}"
+    return {"username": "braghook", "content": f"```{content}```"}
+
+
+def build_discord_webhook(
+    author: str,
+    author_icon: str,
+    content: str,
+) -> dict[str, Any]:
+    """Build the Discord webhook."""
+    title = extract_title_from_message(content)
+    content = bullet_marks_to_diamonds(content)
+    content = headers_to_bold(content)
+
+    return {
+        "username": "braghook",
+        "embeds": [
+            {
+                "author": {
+                    "name": author,
+                    "icon_url": author_icon,
+                },
+                "title": title,
+                "description": content,
+                "color": 0x9C5D7F,
+            },
+        ],
+    }
+
+
+def build_msteams_webhook(
+    author: str,
+    author_icon: str,
+    content: str,
+) -> dict[str, Any]:
+    """Build the MSTeams webhook."""
+    title = extract_title_from_message(content)
+    content = headers_to_bold(content)
+    return {
+        "type": "message",
+        "attachments": [
+            {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "content": {
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "version": "1.2",
+                    "type": "AdaptiveCard",
+                    "themeColor": "9C5D7F",
+                    "body": [
+                        {
+                            "type": "TextBlock",
+                            "text": title,
+                            "size": "medium",
+                            "weight": "bolder",
+                            "style": "heading",
+                        },
+                        {
+                            "type": "ColumnSet",
+                            "columns": [
+                                {
+                                    "type": "Column",
+                                    "width": "auto",
+                                    "items": [
+                                        {
+                                            "type": "Image",
+                                            "url": author_icon,
+                                            "size": "small",
+                                            "style": "person",
+                                            "fallback": "drop",
+                                        }
+                                    ],
+                                },
+                                {
+                                    "type": "Column",
+                                    "width": "stretch",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "text": author,
+                                            "size": "default",
+                                            "weight": "bolder",
+                                            "wrap": True,
+                                        },
+                                        {
+                                            "type": "TextBlock",
+                                            "text": "sent by: braghook",
+                                            "spacing": "none",
+                                            "isSubtle": True,
+                                            "wrap": True,
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": content,
+                            "size": "default",
+                            "weight": "default",
+                            "wrap": True,
+                            "fallback": "drop",
+                            "separator": True,
+                            "id": "contentToToggle",
+                            "isVisible": False,
+                        },
+                    ],
+                    "actions": [
+                        {
+                            "type": "Action.ToggleVisibility",
+                            "title": "Toggle Content",
+                            "targetElements": ["contentToToggle"],
+                        },
+                    ],
+                    "msteams": {
+                        "width": "Full",
+                        "entities": [],
+                    },
+                },
+            }
+        ],
+    }
+
+
 def parse_args(args: list[str] | None = None) -> argparse.Namespace:
     """Parse the arguments."""
     parser = argparse.ArgumentParser()
@@ -213,6 +374,16 @@ def parse_args(args: list[str] | None = None) -> argparse.Namespace:
 def get_input(prompt: str) -> str:
     """Get input from the user."""
     return input(prompt)
+
+
+# Define the builders here, used in the main script
+# NOTE: The key is the config field that defines the url
+# NOTE: The value is the function that builds the message
+BUILDERS: dict[str, Builder] = {
+    "discord_webhook": build_discord_webhook,
+    "discord_webhook_plain": build_discord_webhook_plain,
+    "msteams_webhook": build_msteams_webhook,
+}
 
 
 def main(_args: list[str] | None = None) -> int:
--- tests/braghook_test.py
+++ tests/braghook_test.py
@@ -194,6 +194,83 @@ def test_post_brag_to_gist() -> None:
         )
 
 
+@pytest.mark.parametrize(
+    "message, expected_title",
+    [
+        ("Test message", ""),
+        ("## Test message", "Test message"),
+        ("## Test message \n Test message body", "Test message"),
+    ],
+)
+def test_extract_title_from_message(message: str, expected_title: str) -> None:
+    assert braghook.extract_title_from_message(message) == expected_title
+
+
+@pytest.mark.parametrize(
+    "message, expected_message",
+    [
+        ("Test message", "Test message"),
+        ("## Test message", "## Test message"),
+        ("- Test message", ":small_blue_diamond: Test message"),
+        ("  - Test message", ":small_orange_diamond: Test message"),
+    ],
+)
+def test_bullet_markes_to_diamonds(message: str, expected_message: str) -> None:
+    assert braghook.bullet_marks_to_diamonds(message) == expected_message
+
+
+@pytest.mark.parametrize(
+    "message, expected_message",
+    [
+        ("Test message", "Test message"),
+        ("## Test message", "**Test message**"),
+        ("### Test message", "**Test message**"),
+        ("#### Test message", "**Test message**"),
+        ("##### Test message", "##### Test message"),  # more than four are not headers
+    ],
+)
+def test_headers_to_bold(message: str, expected_message: str) -> None:
+    result = braghook.headers_to_bold(message)
+
+    assert result == expected_message
+
+
+def test_build_discord_webhook() -> None:
+    # Test the results of the webhook by sending it to a Discord channel
+    # this just tests that nothing raises an exception
+    author = "Test Author"
+    author_icon = "https://example.com/icon.png"
+    message = "Test message"
+
+    result = braghook.build_discord_webhook(author, author_icon, message)
+
+    assert result
+
+
+def test_build_plain_discord_webhook() -> None:
+    # Test the results of the webhook by sending it to a Discord channel
+    # this just tests that nothing raises an exception
+    author = "Test Author"
+    author_icon = "https://example.com/icon.png"
+    message = "Test message"
+
+    result = braghook.build_discord_webhook_plain(author, author_icon, message)
+
+    assert result
+
+
+def test_build_msteams_webhook() -> None:
+    # Test the results of the webhook by sending it to a MS Teams channel
+    # this just tests that nothing raises an exception
+    author = "Test Author"
+    author_icon = "https://example.com/icon.png"
+    message = "Test message"
+
+    result = braghook.build_msteams_webhook(author, author_icon, message)
+
+    assert result
+
+
 def test_get_input() -> None:
     with patch("builtins.input") as mock_input:
         mock_input.return_value = "y"
