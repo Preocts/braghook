9db82a4350a656dd449d5e4e9342f6fd26423472
Preocts <preocts@preocts.com>
2023-08-18 19:42:15 -0400

    Clean up noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -3,24 +3,24 @@ from __future__ import annotations
 import nox
 
 MODULE_NAME = "braghook"
-COVERAGE_FAIL_UNDER = "--fail-under=50"
+COVERAGE_FAIL_UNDER = "--fail-under=100"
 
 
 @nox.session(
     python=["3.8", "3.9", "3.10", "3.11", "3.12"],
 )
 def tests_with_coverage(session: nox.Session) -> None:
-    session.log(f"Running from: {session.bin}")
-    session.log(f"Running with: {session.python}")
+    """Run unit tests with coverage saved to partial file."""
+    print_standard_logs(session)
+
     session.install(".[test]")
     session.run("coverage", "run", "-p", "-m", "pytest", "tests/")
 
 
 @nox.session()
 def coverage_combine_and_report(session: nox.Session) -> None:
-    version = session.run("python", "--version", silent=True)
-    session.log(f"Running from: {session.bin}")
-    session.log(f"Running with: {version}")
+    """Combine all coverage partial files and generate JSON report."""
+    print_standard_logs(session)
 
     session.install(".[test]")
     session.run("python", "-m", "coverage", "combine")
@@ -30,10 +30,15 @@ def coverage_combine_and_report(session: nox.Session) -> None:
 
 @nox.session()
 def mypy_check(session: nox.Session) -> None:
-    version = session.run("python", "--version", silent=True)
-    session.log(f"Running from: {session.bin}")
-    session.log(f"Running with: {version}")
+    print_standard_logs(session)
 
     session.install(".")
     session.install("mypy")
     session.run("mypy", "-p", MODULE_NAME, "--no-incremental")
+
+
+def print_standard_logs(session: nox.Session) -> None:
+    """Reusable output for monitoring environment factors."""
+    version = session.run("python", "--version", silent=True)
+    session.log(f"Running from: {session.bin}")
+    session.log(f"Running with: {version}")
